0b1f205eb8f961891f363030b5955a494e102be3
SebasGA19 <62715839+SebasGA19@users.noreply.github.com>
2022-08-24 10:39:48 -0500

    Update graficador_analisis.py

--- graficador_analisis.py
+++ graficador_analisis.py
@@ -7,11 +7,13 @@ Created on Fri Aug 12 13:59:27 2022
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
 import pandas
 from matplotlib import pyplot
 import numpy as np
 import scipy.stats
 import statistics
+from scipy.stats import skew
 
 root = tk.Tk()
 abalone = pandas.read_csv("./abalone.csv")
@@ -128,20 +130,6 @@ def dist_normal(atipicos):
     pyplot.cla()
     pyplot.clf()
     
-def correlacion(atipicos):
-    if(atipicos==False):
-        datos = abalone
-    else:
-        datos = abalone_limpio
-    fig, ax = pyplot.subplots()
-    cax = ax.matshow(datos.corr())
-    fig.colorbar(cax)
-    ax.set_xticklabels(['']+datos.columns, rotation='vertical')
-    ax.set_yticklabels(['']+datos.columns)
-    pyplot.show()
-    pyplot.cla()
-    pyplot.clf()
-    
 def scatter_plot(atipicos):
     nombre_columna = scatter_v.get()
     nombre_columna_vs = scatter_vs_v.get()
@@ -159,6 +147,7 @@ def scatter_plot(atipicos):
     pyplot.show()
     pyplot.cla()
     pyplot.clf()
+    
 
 media_mediana_etc = tk.StringVar()
 media_v = tk.StringVar()
@@ -167,6 +156,7 @@ moda_v = tk.StringVar()
 kurtosis_v = tk.StringVar()
 sesgo_v = tk.StringVar()
 kurtosis_c_v = tk.StringVar()
+asimetria_v = tk.StringVar()
 def datos_estadisticos(atipicos):
     nombre_columna = media_mediana_etc.get()
     if(atipicos==False):
@@ -178,11 +168,13 @@ def datos_estadisticos(atipicos):
     mediana = np.median(column)
     moda = statistics.mode(column)
     kurtosis = scipy.stats.kurtosis(column)
+    asimetria = column.skew()
     
     media_v.set(f"{media}")
     mediana_v.set(f"{mediana}")
     moda_v.set(f"{moda}")
     kurtosis_v.set(f"{kurtosis}")
+    asimetria_v.set(f"{asimetria}")
     
     if(moda < mediana < media):
         sesgo_v.set("Sesgada a la izquierda")
@@ -198,10 +190,12 @@ def datos_estadisticos(atipicos):
     
 
 root.title("Regresion de abalones")
-root.geometry("800x1600")
+root.geometry("1080x1600")
 
 #Graficas datos normales
 #Histograma
+
+
 tk.Label(root, text="Datos normales").place(x=10,y=10)
 tk.Label(root, text="Histograma").place(x=10,y=40)
 ttk.Combobox(root, values=columnas, textvariable=histograma_v).place(x=10,y=70)
@@ -223,10 +217,6 @@ ttk.Combobox(root, values=columnas, textvariable=normal_v).place(x=10,y=340)
 tk.Button(root, text="Graficar", command= lambda: dist_normal(False)).place(x=10,y=370)
 
 
-tk.Label(root, text="Correlacion").place(x=10,y=400)
-tk.Button(root, text="Graficar", command=lambda: correlacion(False)).place(x=10,y=430)
-
-
 tk.Label(root, text="Scatter").place(x=10,y=460)
 ttk.Combobox(root, values=columnas_dispersion, textvariable=scatter_v).place(x=10,y=490)
 tk.Label(root, text="VS:").place(x=10,y=520)
@@ -245,9 +235,11 @@ tk.Label(root,text= "Moda").place(x=10,y=790)
 tk.Label(root,textvariable=moda_v).place(x=10,y=820)
 tk.Label(root,text= "Kurtosis").place(x=10,y=850)
 tk.Label(root,textvariable=kurtosis_v).place(x=10,y=880)
-tk.Label(root,text= "Sesgo y Kurtosis").place(x=10,y=910)
-tk.Label(root,textvariable=sesgo_v).place(x=10,y=940)
-tk.Label(root,textvariable=kurtosis_c_v).place(x=10,y=970)
+tk.Label(root,text= "Asimetria").place(x=400,y=910)
+tk.Label(root,textvariable=asimetria_v).place(x=400,y=940)
+tk.Label(root,text= "Sesgo y Kurtosis").place(x=10,y=970)
+tk.Label(root,textvariable=sesgo_v).place(x=10,y=1000)
+tk.Label(root,textvariable=kurtosis_c_v).place(x=200,y=1000)
 
 
 # Datos sin atipicos
@@ -272,10 +264,6 @@ ttk.Combobox(root, values=columnas, textvariable=normal_v).place(x=400,y=340)
 tk.Button(root, text="Graficar", command= lambda: dist_normal(True)).place(x=400,y=370)
 
 
-tk.Label(root, text="Correlacion").place(x=400,y=400)
-tk.Button(root, text="Graficar", command=lambda: correlacion(True)).place(x=400,y=430)
-
-
 tk.Label(root, text="Scatter").place(x=400,y=460)
 ttk.Combobox(root, values=columnas_dispersion, textvariable=scatter_v).place(x=400,y=490)
 tk.Label(root, text="VS:").place(x=400,y=520)
@@ -294,9 +282,11 @@ tk.Label(root,text= "Moda").place(x=400,y=790)
 tk.Label(root,textvariable=moda_v).place(x=400,y=820)
 tk.Label(root,text= "Kurtosis").place(x=400,y=850)
 tk.Label(root,textvariable=kurtosis_v).place(x=400,y=880)
-tk.Label(root,text= "Sesgo y Kurtosis").place(x=400,y=910)
-tk.Label(root,textvariable=sesgo_v).place(x=400,y=940)
-tk.Label(root,textvariable=kurtosis_c_v).place(x=400,y=970)
+tk.Label(root,text= "Asimetria").place(x=400,y=910)
+tk.Label(root,textvariable=asimetria_v).place(x=400,y=940)
+tk.Label(root,text= "Sesgo y Kurtosis").place(x=400,y=970)
+tk.Label(root,textvariable=sesgo_v).place(x=400,y=1000)
+tk.Label(root,textvariable=kurtosis_c_v).place(x=600,y=1000)
 
 
 root.mainloop()
